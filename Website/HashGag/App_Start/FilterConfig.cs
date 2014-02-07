3b9d0a851e9438ca56ea3e2fcc6d6b07e68b0266
livelyjp <jonny@jonnypillar.co.uk>
2014-02-07 08:26:18 +0000

    Addition of the Website Folder with MVC5 Website included

---
+++ Website/HashGag/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace HashGag
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
