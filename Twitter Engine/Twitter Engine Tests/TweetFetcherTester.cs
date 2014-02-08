5aa43667f7f48cdc1fa2914e01239ba711526e19
brandscill <brandscill@gmail.com>
2014-02-08 17:08:00 +0000

    Created TwitterFetcher and testing framework

---
+++ Twitter Engine/Twitter Engine Tests/TweetFetcherTester.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using TweetinCore.Interfaces;
+using Tweetinvi;
+using Twitter_Engine;
+using Assert = NUnit.Framework.Assert;
+
+namespace Twitter_Engine_Tests
+{
+    [TestClass]
+    public class TweetFetcherTester
+    {
+        [TestFixture]
+        public class TestTweetInvi
+        {
+            [Test]
+            public void doesTweetMatchHashTag()
+            {
+                List<ITweet> matchingTweets = new TwitterFetcher().GetListOfTweetsWithHashTag("nicetrydickheadbob");
+                ITweet tweet = matchingTweets.First();
+                Assert.That(tweet.Hashtags.First().Text, Is.EqualTo("nicetrydickheadbob"));
+            }
+
+            [Test]
+            public void testGetNumberOfTweetsForHashtag()
+            {
+                List<ITweet> matchingTweets = new TwitterFetcher().GetListOfTweetsWithHashTag("nicetrydickheadbob");
+                Assert.That(matchingTweets.Count, Is.GreaterThan(0));
+            }
+        }
+
+        [TestFixture]
+        public class TestTweetManipulation : ITweetFetcher
+        {
+            public List<ITweet> GetListOfTweetsWithHashTag(string hashTag)
+            {
+                var listOfTweets = new List<ITweet>();
+                ITweet tweet = new Tweet();
+                tweet.Creator = new User("Jimmy");
+                listOfTweets.Add(tweet);
+                return listOfTweets;
+            }
+
+            [Test]
+            public void testTwitterUsernamePresence()
+            {
+                string sender = new CompetitionTweet(this).Sender();
+                Assert.That(sender, Is.EqualTo("Jimmy"));
+            }
+        }
+    }
+}
---
+++ Twitter Engine/Twitter Engine/TwitterFetcher.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TweetinCore.Events;
+using TweetinCore.Extensions;
+using TweetinCore.Interfaces;
+using TweetinCore.Interfaces.TwitterToken;
+using Tweetinvi;
+using TwitterToken;
+
+namespace Twitter_Engine
+{
+    public class TwitterFetcher : ITweetFetcher
+    {
+        private IToken token;
+        public TwitterFetcher()
+        {
+            token = new Token(
+            ConfigurationManager.AppSettings["token_AccessToken"],
+            ConfigurationManager.AppSettings["token_AccessTokenSecret"],
+            ConfigurationManager.AppSettings["token_ConsumerKey"],
+            ConfigurationManager.AppSettings["token_ConsumerSecret"]);
+
+        }
+        public List<ITweet> GetListOfTweetsWithHashTag(string hashTag)
+        {
+            List<ITweet> tweetsForHashtag = new List<ITweet>();
+
+            ObjectResponseDelegate responseDelegate = tweetsMatchingQuery =>
+            {
+                foreach (var matchingTweet in StatusesOf(tweetsMatchingQuery))
+                {
+                    tweetsForHashtag.Add(new Tweet(matchingTweet));
+                }
+            };
+
+            token.ExecuteGETQuery(SearchQuery(hashTag), responseDelegate);
+
+            return tweetsForHashtag;
+        }
+
+        private static IEnumerable<Dictionary<string, object>> StatusesOf(Dictionary<string, object> tweetsMatchingQuery)
+        {
+            return tweetsMatchingQuery.GetPropCollection<Dictionary<string, object>>("statuses");
+        }
+
+        private static string SearchQuery(string hashTag)
+        {
+            return String.Format("https://api.twitter.com/1.1/search/tweets.json?q=%23{0}", hashTag);
+        }
+    }
+
+    
+}
