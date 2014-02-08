bf88485e0ee82cb76c7505325aeb68640ce46432
GaryClaret <Gary.Chadwick1@btinternet.com>
2014-02-08 17:00:56 +0000

    Standings added

---
+++ Website/HashGag/Controllers/StandingsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HashGag.Controllers
+{
+    public class StandingsController : Controller
+    {
+        //
+        // GET: /Standings/
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
