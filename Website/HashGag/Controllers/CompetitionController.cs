de4405a3d2eec0f792d4fd613e98f2c83345342d
livelyjp <jonny@jonnypillar.co.uk>
2014-02-08 14:11:33 +0000

    Inclusion of Controllers and Basic Views

---
+++ Website/HashGag/Controllers/CompetitionController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HashGag.Controllers
+{
+    public class CompetitionController : Controller
+    {
+        //
+        // GET: /Competition/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
---
+++ Website/HashGag/Controllers/HubController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HashGag.Controllers
+{
+    public class HubController : Controller
+    {
+        //
+        // GET: /Hub/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+	}
+}
