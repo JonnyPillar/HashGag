a2752aa1139e72ac72ea95095c7bea0b2f6d9726
livelyjp <jonny@jonnypillar.co.uk>
2014-02-09 10:41:40 +0000

    Commuit

--- Website/HashGag/Controllers/CompetitionController.cs
+++ Website/HashGag/Controllers/CompetitionController.cs
@@ -17,42 +17,6 @@ namespace HashGag.Controllers
         // GET: /Competition/
         public ActionResult Index()
         {
-            //List<Question> questionList = new List<Question>();
-
-            //Question sdf = new Question();
-
-            //sdf.Text = "New Question";
-            //sdf.TwitterUser = new TwitterUser();
-            //sdf.TwitterUser.ProfileImageURL = "https://pbs.twimg.com/profile_images/3685227674/67c501ba4eb74c8b7426f1109b38e70a_bigger.jpeg";
-            //sdf.EndDate = new DateTimeOffset(DateTime.Now);
-
-            //questionList.Add(sdf);
-            //Question sdfa = new Question();
-            //sdfa.Text = "New Question Two";
-            //sdfa.TwitterUser = new TwitterUser();
-            //sdfa.TwitterUser.ProfileImageURL = "https://pbs.twimg.com/profile_images/3685227674/67c501ba4eb74c8b7426f1109b38e70a_bigger.jpeg";
-            //sdfa.EndDate = new DateTimeOffset(DateTime.Now);
-            //questionList.Add(sdfa);
-            //Question sdsf = new Question();
-            //sdsf.Text = "New Question Three";
-            //sdsf.TwitterUser = new TwitterUser();
-            //sdsf.TwitterUser.ProfileImageURL = "https://pbs.twimg.com/profile_images/3685227674/67c501ba4eb74c8b7426f1109b38e70a_bigger.jpeg";
-            //sdsf.EndDate = new DateTimeOffset(DateTime.Now);
-            //questionList.Add(sdsf);
-            //Question sddf = new Question();
-            //sddf.Text = "New Question Four";
-            //sddf.TwitterUser = new TwitterUser();
-            //sddf.TwitterUser.ProfileImageURL = "https://pbs.twimg.com/profile_images/3685227674/67c501ba4eb74c8b7426f1109b38e70a_bigger.jpeg";
-            //sddf.EndDate = new DateTimeOffset(DateTime.Now);
-            //questionList.Add(sddf);
-            //Question sfdf = new Question();
-            //sfdf.Text = "New Question Five";
-            //sfdf.TwitterUser = new TwitterUser();
-            //sfdf.TwitterUser.ProfileImageURL = "https://pbs.twimg.com/profile_images/3685227674/67c501ba4eb74c8b7426f1109b38e70a_bigger.jpeg";
-            //sfdf.EndDate = new DateTimeOffset(DateTime.Now);
-            //questionList.Add(sfdf);
-
-            //CompetitionViewModel model = new CompetitionViewModel(questionList, questionList, questionList);
             CompetitionViewModel model = new CompetitionViewModel(null, null, null, null);
             return View(model);
         }
@@ -137,6 +101,15 @@ namespace HashGag.Controllers
             CompetitionViewModel cmodel = new CompetitionViewModel(question1,highestscoringOrderedTweets, myOrdereedTweets, mostRecentOrderedTweets);
 
             return View("Index", cmodel);
+        }
+
+        [HttpPost]
+        public ActionResult LoadColumn(int id)
+        {
+
+
+
+            return View("CompQuestionListView", id);
         }
 	}
 }
