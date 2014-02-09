ae0e568f75b334a7c3241f2f583b8d65be71caa7
livelyjp <jonny@jonnypillar.co.uk>
2014-02-09 11:27:22 +0000

    Themeing

--- Website/HashGag/Controllers/CompetitionController.cs
+++ Website/HashGag/Controllers/CompetitionController.cs
@@ -103,11 +103,98 @@ namespace HashGag.Controllers
             return View("Index", cmodel);
         }
 
+        public ActionResult Details(string id)
+        {
+
+            string[] temp = id.Split(',');
+            id = temp[0];
+
+            int asd = Convert.ToInt32(id);
+
+            hashgagEntities db = new hashgagEntities();
+
+            var query = from q in db.Questions
+                        join cpt in db.CompetitionTweets on q.QuestionID equals cpt.QuestionID
+                        join t in db.Tweets on cpt.TweetID equals t.TweetID
+                        join tu in db.TwitterUsers on t.TwitterUserID equals tu.TwitterID
+                        where q.QuestionID == asd
+
+                        select new { q, t, tu, cpt };
+
+            List<CompTweetModel> list = new List<CompTweetModel>();
+            Question question = null;
+            if (query.Count() == 0) question = new Question();
+            foreach (var item in query)
+            {
+                if (question == null) question = item.q;
+                Tweet tweet = item.t;
+                tweet.RetweetCount++;
+                TwitterUser tUser = item.tu;
+
+                CompTweetModel cTUser = new CompTweetModel(tweet, tUser);
+
+                list.Add(cTUser);
+            }
+
+            List<CompTweetModel> highestscoringOrderedTweets = new List<CompTweetModel>();
+            highestscoringOrderedTweets.AddRange(from atweet in list orderby atweet.tweet.RetweetCount descending select atweet);
+
+
+            if (User.Identity.IsAuthenticated)
+            {
+                int FirstInstanceOfUser = highestscoringOrderedTweets.IndexOf((from alltweets in highestscoringOrderedTweets
+                                                                               where alltweets.user.ScreenName == "bendywalker"
+                                                                               orderby alltweets.tweet.RetweetCount descending
+                                                                               select alltweets).FirstOrDefault());
+
+                int totalNeeded = highestscoringOrderedTweets.Count >= 10
+                    ? 10
+                    : highestscoringOrderedTweets.Count;
+
+
+                if (FirstInstanceOfUser - 5 < 0)
+                {
+                    FirstInstanceOfUser = FirstInstanceOfUser - (FirstInstanceOfUser - 5);
+                }
+                if (FirstInstanceOfUser + 5 > highestscoringOrderedTweets.Count)
+                {
+                    FirstInstanceOfUser = FirstInstanceOfUser - ((FirstInstanceOfUser + 5) - highestscoringOrderedTweets.Count);
+                }
+
+                if (totalNeeded < 10)
+                {
+                    //myOrdereedTweets = highestscoringOrderedTweets;
+                }
+                else
+                {
+                    foreach (CompTweetModel atweetmodel in highestscoringOrderedTweets)
+                    {
+                        if (highestscoringOrderedTweets.IndexOf(atweetmodel) >= (FirstInstanceOfUser - 5)
+                            && highestscoringOrderedTweets.IndexOf(atweetmodel) < (FirstInstanceOfUser + 5))
+                        {
+                            //myOrdereedTweets.Add(atweetmodel);
+                        }
+                    }
+                }
+            }
+            ;
+
+
+            Question question1 = new Question();
+
+            CompetitionViewModel cmodel = new CompetitionViewModel(question1, highestscoringOrderedTweets, null, null);
+
+            return View("Index", cmodel);
+
+
+            return View("CompQuestionListView", cmodel.ColumnOneQuestions);
+        }
+
         [HttpPost]
         public ActionResult LoadColumn(int id)
         {
 
-
+            
 
             return View("CompQuestionListView", id);
         }
